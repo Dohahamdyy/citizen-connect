56bdbd39d1c64cbb14cc788de63e13d8900a42ce
Dohahamdyy <dohahamdy21@icloud.com>
2024-07-03 13:58:12 +0400

    fixed signup screen

--- lib/ui/screens/signup_screen.dart
+++ lib/ui/screens/signup_screen.dart
@@ -22,154 +22,161 @@ class _SignupScreenState extends State<SignupScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                    bottom: 12,
-                    child: Container(
-                      width: 28,
-                      height: 28,
-                      decoration: const BoxDecoration(
-                        color: Colors.amberAccent,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    icon: const Icon(
-                      Icons.keyboard_backspace_rounded,
-                      color: Colors.black,
-                      size: 36,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30),
-              const Row(
-                children: [
-                  SizedBox(width: 12),
-                  Text(
-                    'Sign up',
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Stack(
                   children: [
-                    const SizedBox(height: 30),
-                    TextFormField(
-                      decoration: textFieldDecoration.copyWith(
-                        labelText: 'Name',
+                    Positioned(
+                      bottom: 12,
+                      child: Container(
+                        width: 28,
+                        height: 28,
+                        decoration: const BoxDecoration(
+                          color: Colors.amberAccent,
+                          shape: BoxShape.circle,
+                        ),
                       ),
-                      controller: _nameController,
                     ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: textFieldDecoration.copyWith(
-                        labelText: 'Email',
+                    IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: const Icon(
+                        Icons.keyboard_backspace_rounded,
+                        color: Colors.black,
+                        size: 36,
                       ),
-                      validator: (value) {
-                        if (value!.isEmpty || !emailRegex.hasMatch(value)) {
-                          return 'Email is required';
-                        }
-                        return null;
-                      },
-                      controller: _emailController,
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: textFieldDecoration.copyWith(
-                        labelText: 'Phone number',
-                      ),
-                      controller: _idController,
-                      keyboardType: TextInputType.number,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: textFieldDecoration.copyWith(
-                        labelText: 'Password',
-                      ),
-                      controller: _passwordController,
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      decoration: textFieldDecoration.copyWith(
-                        labelText: 'Confirm Password',
-                      ),
-                      controller: _confirmPasswordController,
-                      obscureText: true,
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Material(
-                    borderRadius: BorderRadius.circular(30),
-                    elevation: 5,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.amberAccent,
+                const SizedBox(height: 30),
+                const Row(
+                  children: [
+                    SizedBox(width: 12),
+                    Text(
+                      'Sign up',
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: MaterialButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            _formKey.currentState!.save();
-                            Navigator.of(context).pushNamed('/main');
+                    ),
+                  ],
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: textFieldDecoration.copyWith(
+                          labelText: 'Name',
+                        ),
+                        controller: _nameController,
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: textFieldDecoration.copyWith(
+                          labelText: 'Email',
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || !emailRegex.hasMatch(value)) {
+                            return 'Email is required';
                           }
+                          return null;
                         },
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 80),
-                          child: Text(
-                            'Create account',
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: textFieldDecoration.copyWith(
+                          labelText: 'Phone number',
+                        ),
+                        controller: _idController,
+                        keyboardType: TextInputType.number,
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: textFieldDecoration.copyWith(
+                          labelText: 'Password',
+                        ),
+                        controller: _passwordController,
+                        obscureText: true,
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: textFieldDecoration.copyWith(
+                          labelText: 'Confirm Password',
+                        ),
+                        controller: _confirmPasswordController,
+                        obscureText: true,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Material(
+                      borderRadius: BorderRadius.circular(30),
+                      elevation: 5,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.amberAccent,
+                        ),
+                        child: MaterialButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+                              Navigator.of(context).pushNamed('/main');
+                            }
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 80),
+                            child: Text(
+                              'Create account',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 15),
-              Center(
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Already have an account? ',
-                    style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                    children: [
-                      TextSpan(
-                        text: 'Login',
-                        style: const TextStyle(
-                          color: AppColors.yellowColor,
+                  ],
+                ),
+                const SizedBox(height: 15),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Already have an account? ',
+                      style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                          text: 'Login',
+                          style: const TextStyle(
+                            color: AppColors.yellowColor,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Navigator.of(context).pop(),
                         ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => Navigator.of(context).pop(),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
